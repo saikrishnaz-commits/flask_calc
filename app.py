7ebd298245ff1a3dbfcca07153274e2295237c6d
Sai Krishna <saikrishna9770@gmail.com>
2025-09-21 19:20:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,5 @@ def home():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))   # default to 5000 if not set
+    app.run(host="0.0.0.0", port=port, debug=True
